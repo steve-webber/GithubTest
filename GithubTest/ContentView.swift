c0711218ca85243da95150390692f883b0cbfa4a
Steve Webber <steve@gibbethill.com>
2026-06-25 11:48:35 -0400

    Still learning

--- GithubTest/ContentView.swift
+++ GithubTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, there!")
+            Text("Hello, there, Steve!")
         }
         .padding()
     }
